4f7b39a55120c2ca8b0ebdc1947b27be010bfd30
AndresMRM <andres@inventati.org>
2016-11-15 09:15:48 -0200

    os

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='andres@inventati.org',
     url='https://github.com/andresmrm/promise-tracker-app',
     packages=["tracker"],
-    install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+    # install_requires=['Flask>=0.7.2', 'MarkupSafe'],
     keywords=[],
     classifiers=[]
 )
